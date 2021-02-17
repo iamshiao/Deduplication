8f0d178069a24a2610bd54fe7683e5890604a674
joseph351619 <sungfuchiu@gmail.com>
2021-02-17 21:26:15 +0800

    Draft of BFBC

---
+++ Deduplication.Controller/Algorithm/BFBC.cs
@@ -0,0 +1,88 @@
+﻿using Deduplication.Controller.Extensions;
+using Deduplication.Model.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Deduplication.Controller.Algorithm
+{
+
+    public class BFBC : DeduplicationAlgorithm
+    {
+        private readonly int _minT, _maxT;
+
+        public BFBC(int minT, int maxT, Action<ProgressInfo, string> updateProgress = null) : base(updateProgress)
+        {
+            _minT = minT;
+            _maxT = maxT;
+        }
+
+        public override IEnumerable<Chunk> Chunk(byte[] bytes)
+        {
+            HashSet<Chunk> chunks = new HashSet<Chunk>();
+
+
+            UpdateChunkingProgress("Start chuncking", 0, bytes.Length);
+
+            if (bytes.Length <= _minT)
+            {
+                string chunkID = ComputeTripleHash(bytes);
+                var chunk = new Chunk()
+                {
+                    Id = chunkID,
+                    Bytes = bytes
+                };
+                chunks.Add(chunk);
+                UpdateChunkingProgress("Finished chunking", bytes.Length, bytes.Length);
+            }
+            else
+            {
+                byte[] divisors = GetDivisorsByFrequency(bytes);
+                int lastP = 0, bp = 0;
+                for (int i=0; i<bytes.Length; ++i)
+                {
+                    if (i + 2 == bytes.Length)
+                    {
+                        string chunkID = ComputeTripleHash(bytes);
+                        var chunk = new Chunk()
+                        {
+                            Id = chunkID,
+                            Bytes = bytes
+                        };
+                        chunks.Add(chunk);
+                    }
+                    else
+                    {
+                        if (bytes[i] == divisors[0]
+                                && bytes[i + 1] == divisors[1])
+                        {
+                            bp = i + 2;
+                            var piece = bytes.SubArray(bp, bp - lastP);
+                            string chunkID = ComputeTripleHash(piece);
+                            var chunk = new Chunk()
+                            {
+                                Id = chunkID,
+                                Bytes = piece
+                            };
+                            chunks.Add(chunk);
+                            lastP = bp;
+                            UpdateChunkingProgress("Chunking", bp, bytes.Length);
+                        }
+                    }
+                }
+                UpdateChunkingProgress("Finished chunking", bytes.Length, bytes.Length);
+            }
+            return chunks;
+        }
+
+        private byte[] GetDivisorsByFrequency(byte[] bytes)
+        {
+            return new byte[] { 0, 1 };
+        }
+
+        private string ComputeTripleHash(byte[] bytes)
+        {
+            return bytes.GetHashCode().ToString();
+        }
+    }
+}
